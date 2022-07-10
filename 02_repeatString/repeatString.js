76d44ccfa52dde1ccb9cfc6c23ef33651582e07e
eric <ericbushnell@gmail.com>
2022-07-10 19:43:49 -0400

    Improve solution to exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,10 @@
-const repeatString = function(string, num) {
-  newString = string;
-
-  if (num < 0){
-    return "ERROR";
-  } else if (num === 0){
-    return "";
-  } else {
-    for (let i = 1; i < num; i++){
-      newString += string;
-    }
-    return newString;
+const repeatString = function(word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++){
+    string += word;
   }
+  return string;
 };
 
 // Do not edit below this line
