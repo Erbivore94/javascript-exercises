183db29774f71680593230df1c9daed6b6e48232
eric <ericbushnell@gmail.com>
2022-07-10 19:38:00 -0400

    Complete exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  newString = string;
 
+  if (num < 0){
+    return "ERROR";
+  } else if (num === 0){
+    return "";
+  } else {
+    for (let i = 1; i < num; i++){
+      newString += string;
+    }
+    return newString;
+  }
 };
 
 // Do not edit below this line
