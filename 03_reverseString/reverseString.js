ad072e7813b9900d16586d8c3315492f9e2947ff
eric <ericbushnell@gmail.com>
2022-07-10 21:47:06 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(text) {
+  array = text.split("");
+  reverse = array.reverse();
+  join = reverse.join();
+  noComma = join.replace(/,/g, ""); //g for "global," capturing all commas
+  return noComma;
+}
+console.log(reverseString("hello there"));
 // Do not edit below this line
 module.exports = reverseString;
