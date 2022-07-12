7d058091ba1f5fcda5d62514307502ff814334d3
eric <ericbushnell@gmail.com>
2022-07-12 18:01:01 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * (5/9);
+  if (Number.isInteger(c)) return c;
+  else {
+    let rounded = c.toFixed(1);
+    rounded = parseFloat(rounded);
+    return rounded;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * (9/5) + 32;
+  if (Number.isInteger(f)) return f;
+  else {
+    let rounded = f.toFixed(1);
+    rounded = parseFloat(rounded);
+    return rounded;
+  }
 };
 
 // Do not edit below this line
