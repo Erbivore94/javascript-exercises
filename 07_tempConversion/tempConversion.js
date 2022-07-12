4ead6fc5782103f52ede5b90e70ddf66aa78f518
eric <ericbushnell@gmail.com>
2022-07-12 18:05:57 -0400

    Improve solution to exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,21 +1,9 @@
 const ftoc = function(f) {
-  let c = (f - 32) * (5/9);
-  if (Number.isInteger(c)) return c;
-  else {
-    let rounded = c.toFixed(1);
-    rounded = parseFloat(rounded);
-    return rounded;
-  }
+  return Math.round((f - 32) * (5/9) * 10) / 10;
 };
 
 const ctof = function(c) {
-  let f = c * (9/5) + 32;
-  if (Number.isInteger(f)) return f;
-  else {
-    let rounded = f.toFixed(1);
-    rounded = parseFloat(rounded);
-    return rounded;
-  }
+  return Math.round(((c * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
