3f027b6a1435d5aa5cec90ee8204d0c83c97b337
eric <ericbushnell@gmail.com>
2022-07-12 17:05:34 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (a > b) {
+    min = b;
+    max = a;
+  } else {
+    min = a;
+    max = b;
+  }
 
+  let array = [];
+  for (let i = min; i <= max; i++) {
+    array.push(i);
+  }
+  let total = 0;
+  for (num of array) {
+    total+= num;
+  }
+  return total;
 };
 
 // Do not edit below this line
